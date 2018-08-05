3b8ef2978b172764e5d0a1b2140a316b7429e32b
luna <yljloveliucan@126.com>
2018-08-05 18:15:10 +0800

    高并发项目实战初始化

---
+++ src/main/java/com/mall/concurrcy/ConcurrcyApplication.java
@@ -0,0 +1,12 @@
+package com.mall.concurrcy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrcyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrcyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mall/concurrcy/ConcurrcyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mall.concurrcy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurrcyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
